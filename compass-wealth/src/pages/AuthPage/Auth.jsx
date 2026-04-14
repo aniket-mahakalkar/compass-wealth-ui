bcdf1ff6ea871728b754c5502fb3f8656e12771e
AniketSynLabs <aniket.mahakalkar@synlabs.io>
2026-04-14 17:36:37 +0530

    feat: initialize React project with Vite, Tailwind CSS, and React Router

---
+++ compass-wealth/src/pages/AuthPage/Auth.jsx
@@ -0,0 +1,15 @@
+const Auth = () => {
+    return (
+
+        <div className="flex h-screen w-full justify-center items-center bg-black">
+
+            <div className="text-white">
+                <h1>Auth Page</h1>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Auth;
