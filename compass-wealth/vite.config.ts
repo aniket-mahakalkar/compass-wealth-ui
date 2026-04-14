dba256a2a0ce2acf6c7eee886a3f4abe94067ed4
AniketSynLabs <aniket.mahakalkar@synlabs.io>
2026-04-14 17:54:08 +0530

    added shadcn

--- compass-wealth/vite.config.ts
+++ compass-wealth/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -9,6 +10,11 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss() 
   ],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
